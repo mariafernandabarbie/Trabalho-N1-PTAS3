a6fb8aafcdbe7d699bd789e84461e005ffd22ed4
CaioHPerlin <maria.souza27@estudante.ifms.edu.br>
2023-10-19 21:28:56 -0400

    ff

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,14 @@
 const User = require('../models/User');
 const secret = require('../config/auth.json');
 const jwt = require('jsonwebtoken');
+const newpassword = await bcrypt.hash(password, 10);
+
 
 const createUser = async (req, res) => {
-    const { name, password, email } = req.body;
+    const { name, email } = req.body;
     await User.create({
         name: name,
-        password: password,
+        password: newpassword,
         email: email
     }).then(() => {
         res.json('Cadastro de usuário realizado com sucesso!');
@@ -19,70 +21,72 @@ const createUser = async (req, res) => {
 
 const searchUsers = async (req, res) => {
     const users = await User.findAll()
-       try{
+    try {
         res.json(users);
-       }
-       catch(error){
-            res.status(404).json("Deu erro")
-       }
+    }
+    catch (error) {
+        res.status(404).json("Deu erro")
+    }
 }
 
 
-const deleteUser = async(req, res) =>{
+const deleteUser = async (req, res) => {
     const id = parseInt(req.params.id);
     try {
         await User.destroy({
-            where:{
-                id:id
+            where: {
+                id: id
             }
-    }).then(()=>{
-        res.json("Usuário deletado")
-    })
-} catch(error){
-    res.status(404).json("Deu erro!")
-}
+        }).then(() => {
+            res.json("Usuário deletado")
+        })
+    } catch (error) {
+        res.status(404).json("Deu erro!")
+    }
 }
 
-const updateUser = async(req, res) =>{
+const updateUser = async (req, res) => {
     const id = parseInt(req.params.id);
     const { name, password, email } = req.body;
     try {
         await User.update({
-        name: name,
-        password: password,
-        email:email
-    },
-    {
-        where: {
-        id:id
+            name: name,
+            password: password,
+            email: email
+        },
+            {
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.json("Usuário alterado!")
+            })
     }
-    }).then(()=>{
-        res.json("Usuário alterado!")
-    })
-}
-    catch(error){
-          res.status(404).json("Deu erro!")
-}
+    catch (error) {
+        res.status(404).json("Deu erro!")
+    } 
 }
 
-const authenticatedUser = async (req, res) =>{
-    const {email, password} = req.body;
+const authenticatedUser = async (req, res) => {
+    const { email, password } = req.body;
     try {
+        const response = await bcrypt.compare(password, passwordCrypto);
         const isUserAthenticated = await User.findOne({
-            where:{
+            where: {
                 email: email,
-                password:password
+                password: newpassword,
+                password: response
             }
         })
-        const token = jwt.sign({id:email}, secret.secret, {
-            expiresIn:86400,
+        const token = jwt.sign({ id: email }, secret.secret, {
+            expiresIn: 86400,
         })
         return res.json({
             name: isUserAthenticated.name,
             email: isUserAthenticated.email,
             token: token
         });
-    }catch (error){
+    } catch (error) {
         return res.json('Usuário nao encontrado');
     }
 }
